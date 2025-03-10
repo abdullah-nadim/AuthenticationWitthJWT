871e86db1cb700164d7391a38a7faccf84d07dae
abdullah-nadim <nadim.soft.111@gmail.com>
2025-03-10 14:42:09 +0600

    Add project files.

---
+++ AuthenticationWitthJWT/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using AuthenticationWitthJWT.DTOs;
+using AuthenticationWitthJWT.Entitys;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationWitthJWT.Controllers
+{
+    public class AuthController : Controller
+    {
+        public static  User _user = new();
+        //[Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Registration() 
+        { 
+            return View(); 
+        }
+
+
+        [HttpPost]
+        public IActionResult Registration( UserDto userDto)
+        {
+            if (!ModelState.IsValid) return View(userDto); 
+
+            var hasher = new PasswordHasher<User>();
+            _user.Username = userDto.Username;
+            _user.Password = hasher.HashPassword(_user, userDto.Password);
+
+            return View("RegistrationSuccess", _user);
+        }
+    }
+}
